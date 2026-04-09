d253b8b8328757dd60692c675a75aafdb89c071d
Raj Mishra <raj212558@gmail.com>
2026-04-09 08:10:16 +0530

    adding signup routes

---
+++ src/app/api/user/signup/route.ts
@@ -0,0 +1,43 @@
+import { connect } from "@/dbconfig/dbConfig";
+import User from "@/models/userModels";
+import bcryptjs from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    await connect();
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 422 },
+      );
+    }
+
+    // hash password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+
+    return NextResponse.json({
+      message: "User registered successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.error("Error during user signup:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
